f2b4d4d31dce5cc21154c1f936a15e089aec96a9
D-trisakti <denytrisakti@gmail.com>
2021-04-26 23:50:40 +0700

    interface

---
+++ application/views/index.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <div class="row text-center">
+        <div class="col-3">
+            <div class="card">
+            <img class="card-img-top rounded mx-auto d-block" src="<?= base_url()?>/assets/img/consulting.png" alt="Card image cap" style ="width : 3rem ; height : 3rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Konsultasi Dengan Bidan</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+            <img class="card-img-top rounded mx-auto d-block" src="<?= base_url()?>/assets/img/consulting.png" alt="Card image cap" style ="width : 3rem ; height : 3rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Konsultasi Dengan Bidan</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+            <img class="card-img-top rounded mx-auto d-block" src="<?= base_url()?>/assets/img/consulting.png" alt="Card image cap" style ="width : 3rem ; height : 3rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Konsultasi Dengan Bidan</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+            <img class="card-img-top rounded mx-auto d-block" src="<?= base_url()?>/assets/img/consulting.png" alt="Card image cap" style ="width : 3rem ; height : 3rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Konsultasi Dengan Bidan</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
